94f842c965e363ba6596127224ff980131b51c74
Psveshe4ka <vovarepin@gmail.com>
2024-12-21 18:50:43 +0200

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+public class Main{
+    private static final double CONV_K = 2.20462;
+
+    public static void main (String [] args){
+        System.out.println("App for measure converting");
+        System.out.println("Version 1.0");
+        double kgs = 5;
+        double pnds = 12;
+        double pounds = convKgsToPounds(kgs);
+        double kilos = convPoundsToKgs(pnds);
+        System.out.println("Result is "+ pounds + " pounds and " + kilos +" kgs.");
+    }
+    private static double convKgsToPounds (double kgs){
+        return kgs * CONV_K;
+    }
+    private static  double convPoundsToKgs (double pnds){
+        return  pnds / CONV_K;
     }
 }
