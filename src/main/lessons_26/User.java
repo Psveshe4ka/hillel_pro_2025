37c381b4c1c3fae0130f11c64faf71dbffa046ca
Psveshe4ka <vovarepin@gmail.com>
2025-04-10 11:07:30 +0300

    lessons 26

---
+++ src/main/lessons_26/User.java
@@ -0,0 +1,23 @@
+package main.lessons_26;
+
+public class User {
+    private Address address;
+    private String name;
+    public User(String name) {
+        this.name = name;
+    }
+    public Address getAddress() {
+        return address;
+    }
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
